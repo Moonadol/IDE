a7c036840846c61cb5c1193d8f114cb2b444bee5
moonnoo <nkv20.10.2000@gmail.com>
2025-01-01 15:50:09 +0300

    IDE

---
+++ проверки/проверка.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Исходное число
+number = -456
+
+# Преобразование в беззнаковый 8-битный целый тип
+uint8_number = np.uint8(number)
+
+# Вывод результата
+print(uint8_number)
